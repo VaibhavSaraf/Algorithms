9b007d055b04baf47b1d26c0aa6b4200d1547dc5
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-05-10 18:24:37 +0530

    new file:   data_structures/a.out 	new file:   data_structures/output.txt 	new file:   data_structures/template.cpp

---
+++ data_structures/template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+#define ff first
+#define ss second
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define maxh priority_queue<int>
+#define minh priority_queue<int, vi, greater<int>>
+#define mod 1000000007
+#define INF 1e18
+void fastio()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+}
+
+int32_t main()
+{
+    fastio();
+    printf("hello world printf");
+    cout<<"hello world"<<endl;
+    int x = 10;
+    cout<<x<<endl;
+    return 0;
+}
