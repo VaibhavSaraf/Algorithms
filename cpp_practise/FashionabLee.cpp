0cbcf71fe42d0ce773c1c3365427d11ec4523aa7
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-06-24 17:35:49 +0530

    new file:   cpp_practise/FashionabLee.cpp 	modified:   cpp_practise/a.out 	new file:   cpp_practise/watermelon.cpp

---
+++ cpp_practise/FashionabLee.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        ll n;
+        cin>>n;
+        if(n%2)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+        
+    }
+    return 0; 
+}
---
+++ cpp_practise/watermelon.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll n;
+    cin>>n;
+    if(n%4)
+    {
+        cout<<"NO";
+    }
+    else
+    {
+        cout<<"YES";
+    }
+    return 0;
+}
