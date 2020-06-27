b695d25da5592764987cd11081cade611e655fdc
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-06-28 00:34:19 +0530

    new file:   cpp_practise/dsa_learning_codechef/coin_flip.cpp

---
+++ cpp_practise/dsa_learning_codechef/coin_flip.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tt;
+    cin>>tt;
+    while(tt--)
+    {
+        int g;
+        cin>>g;
+        while (g--)
+        {
+            int n,p,q;
+            cin>>p>>n>>q;
+            int arr[n];
+            memset(arr,p,sizeof(arr));
+
+        }
+        
+    }
+    return 
+}
