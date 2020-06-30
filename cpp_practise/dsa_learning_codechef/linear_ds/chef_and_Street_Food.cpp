3a6347c311b391448da3e1a61ab13e082741eee3
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-06-30 22:57:30 +0530

    solved

---
+++ cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        int n;
+        cin>>n;
+        int arr[n][3];
+        int c_s;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
+        }
+    }
+    
+}
