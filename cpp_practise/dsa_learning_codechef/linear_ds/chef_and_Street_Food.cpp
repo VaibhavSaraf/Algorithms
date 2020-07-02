78ef651be02dac6050cb9620a4dac891ebe4dbbf
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-07-02 20:58:41 +0530

    new file:   Cracking the Coding Interview, 6th Edition 189 Programming Questions and Solutions.pdf 	new file:   book.pdf 	modified:   cpp_practise/dsa_learning_codechef/.vscode/settings.json 	new file:   cpp_practise/dsa_learning_codechef/linear_ds/a.out 	modified:   cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp

--- cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp
+++ cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp
@@ -8,12 +8,25 @@ int main()
     {
         int n;
         cin>>n;
+        
         int arr[n][3];
-        int c_s;
+        int p;
+        int ans = -1;
         for(int i=0;i<n;i++)
         {
             cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
+            if(arr[i][1]*arr[i][2]>ans)
+            {
+                p=i;
+                ans=arr[i][1]*arr[i][2];
+            }
         }
+        if(n==1)
+        {
+            cout<<0<<endl;
+            break;
+        }
+        cout<<ans/(n+1)<<endl;
     }
     
 }
