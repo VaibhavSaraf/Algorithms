123a45db65280c9fbf9fa08f9e1a6540e2afb764
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-07-03 18:10:46 +0530

    new file:   cpp_practise/dsa_learning_codechef/linear_ds/.vscode/settings.json 	modified:   cpp_practise/dsa_learning_codechef/linear_ds/a.out 	modified:   cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp

--- cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp
+++ cpp_practise/dsa_learning_codechef/linear_ds/chef_and_Street_Food.cpp
@@ -6,27 +6,22 @@ int main()
     cin>>tt;
     while (tt--)
     {
-        int n;
+        long long n;
         cin>>n;
         
-        int arr[n][3];
-        int p;
-        int ans = -1;
-        for(int i=0;i<n;i++)
+        long long arr[n][3];
+        long long p;
+        long long ans = -1;
+        for(long long i=0;i<n;i++)
         {
             cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
-            if(arr[i][1]*arr[i][2]>ans)
+            if((arr[i][1]/(arr[i][0]+1))*arr[i][2]>ans)
             {
                 p=i;
-                ans=arr[i][1]*arr[i][2];
+                ans=(arr[i][1]/(arr[i][0]+1))*arr[i][2];
             }
         }
-        if(n==1)
-        {
-            cout<<0<<endl;
-            break;
-        }
-        cout<<ans/(n+1)<<endl;
+        cout<<ans<<endl;
     }
     
 }
