db088c8a024f88c9beb9d6dc456d03a21f553d82
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-07-03 20:39:42 +0530

    new file:   cpp_practise/dsa_learning_codechef/linear_ds/penalty_Shoot-Out_II.cpp

---
+++ cpp_practise/dsa_learning_codechef/linear_ds/penalty_Shoot-Out_II.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+using namespace std;
+int main()
+{
+    fast int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        ll n;
+        cin >> n;
+        string s;
+        cin >> s;
+        
+    }
+
+    return 0;
+}
