fcdf4cdefbcf1ca98f3e87c313bc563ca16a9103
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-07-04 18:11:19 +0530

    modified:   cpp_practise/dsa_learning_codechef/linear_ds/a.out 	modified:   cpp_practise/dsa_learning_codechef/linear_ds/matched_Brackets.cpp

--- cpp_practise/dsa_learning_codechef/linear_ds/matched_Brackets.cpp
+++ cpp_practise/dsa_learning_codechef/linear_ds/matched_Brackets.cpp
@@ -7,12 +7,49 @@
 using namespace std;
 int main()
 {
-    fast int tt;
-    cin >> tt;
-    while (tt--)
+    fast int n;
+    cin >> n;
+    int s[n];
+    int nestingDepth = 0, firstNestingDepth, lenghtMax = 0, firstLenghtMax;
+    stack<int> v;
+    vector<int> t;
+    int i = 0;
+    for (i = 0; i < n; i++)
     {
-        
+        cin >> s[i];
+        if (s[i] == 1)
+        {
+            v.push(1);
+        }
+        else
+        {
+            if (!v.empty())
+            {
+                v.pop();
+            }
+        }
+        if (nestingDepth < v.size())
+        {
+            nestingDepth = v.size();
+            firstNestingDepth = i + 1;
+        }
+        if (v.empty())
+        {
+            t.push_back(i + 1);
+        }
     }
-
+    for (int i = 0; i < t.size() - 1; i++)
+    {
+        if (lenghtMax < (t[i + 1] - t[i]))
+        {
+            lenghtMax = t[i + 1] - t[i];
+            firstLenghtMax = t[i] + 1;
+        }
+    }
+    // cout<<"nestingDepth: "<<nestingDepth<<"\n";
+    // cout<<"firstNestingDepth: "<<firstNestingDepth<<"\n";
+    // cout<<"lenghtMax: "<<lenghtMax<<"\n";
+    // cout<<"firstLenghtMax: "<<firstLenghtMax<<"\n";
+    cout << nestingDepth << " " << firstNestingDepth << " " << lenghtMax << " " << firstLenghtMax << "\n";
     return 0;
 }
