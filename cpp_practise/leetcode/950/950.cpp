2064ce2a782e326633020f704674ca50fa1b34aa
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-08-04 02:28:03 +0530

    new file:   cpp_practise/leetcode/950/950.cpp 	new file:   cpp_practise/leetcode/950/a.out 	new file:   cpp_practise/leetcode/950/input.txt 	new file:   cpp_practise/leetcode/950/output.txt

---
+++ cpp_practise/leetcode/950/950.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define Time srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+int main()
+{
+    fast OJ int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    sort(v.rbegin(), v.rend());
+    deque<int> q;
+    q.push_front(v[0]);
+    for (int i = 1; i < n; i++)
+    {
+        rotate(q.begin(), q.begin() + q.size() - 1, q.end());
+        q.push_front(v[i]);
+    }
+    q.
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
