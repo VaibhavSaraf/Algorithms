c23b511360a57cef259470cf29647afa76b90b17
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-08-09 19:50:10 +0530

    new file:   cpp_practise/leetcode/67_add_binary_string/1.cpp 	new file:   cpp_practise/leetcode/67_add_binary_string/2.cpp 	new file:   cpp_practise/leetcode/67_add_binary_string/a.out 	new file:   cpp_practise/leetcode/67_add_binary_string/input.txt 	new file:   cpp_practise/leetcode/67_add_binary_string/output.txt

---
+++ cpp_practise/leetcode/67_add_binary_string/1.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+int main()
+{
+    OJ;
+    string a, b;
+    cin>>a>>b;
+    // cout<<a+b<<"\n";
+    int i, j;
+    string s;
+    i = a.length()-1;
+    j = b.length()-1;
+    while (i>=0 && j>=0)
+    {
+            
+        
+    }
+    
+    
+    // s.push_back('a');
+    // s.insert(0,"b");
+    // s.append("vaibhav");
+
+    cout<<s<<"\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
