127501337019ef74e68fb26b4bcea9ed3a59304a
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-08-19 17:02:38 +0530

    new file:   cpp_practise/stl/map_set/1.cpp 	new file:   cpp_practise/stl/map_set/input.txt 	new file:   cpp_practise/stl/map_set/output.txt

---
+++ cpp_practise/stl/map_set/1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+void sol()
+{
+	
+}
+
+
+
+
+
+int main()
+{
+	OJ;
+	sol();
+ 	cout<<"hello world\n";
+
+	return 0;
+}
