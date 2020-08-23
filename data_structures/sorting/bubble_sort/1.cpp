ba275e97d7c2381322c5a20c5ba7278c1ec19c5c
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-08-23 11:35:32 +0530

    a

---
+++ data_structures/sorting/bubble_sort/1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define deb(x) cout << #x << "=" << x << endl
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	
+}
+
+int main()
+{
+	OJ;
+	int tt = 1;
+	// cin>>tt;
+	while(tt--)
+	{
+		solve();
+	}
+	return 0;	
+}
