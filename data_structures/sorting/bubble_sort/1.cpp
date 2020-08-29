9eb0a6e7f0751caaba007e176cf1c51e682423d7
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-08-29 18:58:44 +0530

    a

--- data_structures/sorting/bubble_sort/1.cpp
+++ data_structures/sorting/bubble_sort/1.cpp
@@ -1,9 +1,67 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <iostream>
 #define OJ \
     freopen("input.txt", "r", stdin); \
     freopen("output.txt", "w", stdout);
 #define deb(x) cout << #x << "=" << x << endl
 using namespace std;
+
+void printArray(int *arr, int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
+
+void swap(int &x, int &y)
+{
+	int tmp = x;
+	x = y;
+	y = tmp;
+}
+
+void bubblesort(int arr[],int n)
+{
+	bool flag = 0;
+	int c = 0;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i;j<n-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				swap(arr[j],arr[j+1]);
+				flag = 1;
+			}
+			c++;
+		}
+		if(!flag)
+		{
+			break;
+		}
+		flag = 0;
+	}
+	deb(c);
+	printArray(arr,n);
+}
+
+void selectionSort(int arr[], int n)
+{
+	int min_i = INT32_MAX;
+	int minn = INT32_MAX;
+	for(int i=0;i<n-1;i++)
+	{
+		
+		for(int j=i+1;j<n;j++)
+		{
+			
+		}
+		swap(arr[i],minn);
+	}
+	printArray(arr,n);
+}
 
 void solve()
 {
@@ -12,7 +70,8 @@ void solve()
 	int arr[n];
 	for(int i=0;i<n;i++)
 		cin>>arr[i];
-	
+	// bubblesort(arr,n);
+	selectionSort(arr,n);
 }
 
 int main()
---
+++ data_structures/sorting/bubble_sort/2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define deb(x) cout << #x << "=" << x << endl
+#define ll long long
+using namespace std;
+
+void solve()
+{
+	ll m,n;
+	cin>>m>>n;
+	
+
+}
+
+
+
+
+
+
+
+
+
+int main()
+{
+	OJ;
+	int tt; cin>>tt;
+	while(tt--)
+	{
+		solve();
+	}
+	return 0;
+}
