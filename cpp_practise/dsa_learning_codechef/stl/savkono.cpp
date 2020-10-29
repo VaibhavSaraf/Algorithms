0983b2a3b7f05a5e3537b0b8f95d59f7b1cc2082
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-10-29 19:34:01 +0530

    lc

---
+++ cpp_practise/dsa_learning_codechef/stl/savkono.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define Time srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+void solve()
+{
+    int n, z;
+    cin >> n;
+    cin >> z;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a.rbegin(), a.rend());
+    int ans = 0;
+    int i = 0;
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]<z)
+        {
+            
+        }
+    }
+    cout<<ans<<"\n";
+    return;
+}
+int main()
+{
+    fast;
+    OJ;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ cpp_practise/leetcode/maxDistClosestPer.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define Time srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+int maxDistToClosest(vector<int> &seats)
+{
+
+}
+int main()
+{
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> seats(n);
+        for(int i=0;i<)
+        maxDistToClosest(seats);
+    }
+    return 0;
+}
