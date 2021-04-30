8fb9da7194788309baf3a73ef3bd39ddebd09b75
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2021-04-30 11:14:13 +0530

    1

--- data_structures/tree/isTree.cpp
+++ data_structures/tree/isTree.cpp
@@ -3,42 +3,47 @@ using namespace std;
 #define OJ                            \
     freopen("input.txt", "r", stdin); \
     freopen("output.txt", "w", stdout);
-
 int V, E;
 vector<vector<int>> adj;
 vector<bool> visited;
-vector<int> dist;
 
-void dfs(int node, int height)
+void dfs(int node)
 {
     visited[node] = true;
-    dist[node] = height;
     for (auto child : adj[node])
     {
         if (!visited[child])
-        {
-            dfs(child, dist[node] + 1);
-        }
+            dfs(child);
     }
 }
+
 int main()
 {
-    OJ;
+    // OJ;
     cin >> V >> E;
     adj.resize(V + 1);
-    dist.resize(V + 1);
-    visited.resize(V + 1);
-    fill(visited.begin(), visited.end(), 0);
-    fill(dist.begin(), dist.end(), 0);
+    visited.resize(V + 1, 0);
     int u, v;
-    for (int i = 0; i <= V; i++)
+    for (int i = 0; i < E; i++)
     {
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    dfs(2, 0);
-    for (auto it : dist)
-        cout << it << " ";
+    int cc = 0;
+    for (int i = 1; i <= V; i++)
+    {
+        if (visited[i] == 0)
+        {
+            dfs(i);
+            cc++;
+        }
+    }
+
+    if (cc == 1 && V - 1 == E)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
     return 0;
 }
